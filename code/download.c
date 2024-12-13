bc5ee45019d1b257d4057171668a29e1ac4951f1
Guilherme Silveira Rego <up202207041@up.pt>
2024-12-13 23:31:38 +0000

    download complete (for now)

--- code/download.c
+++ code/download.c
@@ -9,9 +9,15 @@
 
 #define MAX_BUFFER_SIZE 256
 
-#define SERVER_READY 331
+#define SERVER_READY_FOR_PASSWORD 331
 #define SERVER_LOGINSUCCESS 230
 #define SERVER_READY_FOR_AUTH 220
+#define SERVER_PASSIVE 227
+#define SERVER_READY_FOR_DOWNLOAD 150
+#define SERVER_DOWNLOAD_SUCCESS 226
+#define SERVER_EXIT 221
+
+#define PASSIVE "%*[^(](%d,%d,%d,%d,%d,%d)%*[^\n$)]"
 
 typedef struct {
     char user[MAX_BUFFER_SIZE];
@@ -155,6 +161,8 @@ int read_response(const int socket, char* buffer) {
     }
 
     sscanf(buffer, "%d", &responseCode);
+    // Get the response code
+
     return responseCode;
 }
 
@@ -163,30 +171,88 @@ int authenticate(int sockfd, const char *user, const char *password) {
     char userCommand[5+strlen(user)+1]; sprintf(userCommand, "user %s\n", user);
     char passCommand[5+strlen(password)+1]; sprintf(passCommand, "pass %s\n", password);
     char answer[MAX_BUFFER_SIZE];
-    
+
+    // Server is ready, send the user command again
     write(sockfd, userCommand, strlen(userCommand));
     int response = read_response(sockfd, answer);
+    printf("Username Sent! Response: %d\n", response); // 331 Ready For Password
 
-    // Handle the initial server response
-    if (response == SERVER_READY_FOR_AUTH) {
-        // Server is ready, send the user command again
-        write(sockfd, userCommand, strlen(userCommand));
-        response = read_response(sockfd, answer);
-    }
-
-    if (response == SERVER_READY) {
+    if (response == SERVER_READY_FOR_PASSWORD) {
         // Send the password command
         write(sockfd, passCommand, strlen(passCommand));
         response = read_response(sockfd, answer);
     }
+    printf("Password Sent! Response: %d\n", response); // 230: Login Succsessful
 
-    if (response != SERVER_READY) {
+    if (response != SERVER_LOGINSUCCESS) {
         printf("ERROR authenticate: %s\n", answer);
         exit(-1);
     }
 
-    write(sockfd, passCommand, strlen(passCommand));
+    return response;
+}
+
+int passive_mode(int sockfd, char *ip, int *port) {
+    // PASV command
+    char answer[MAX_BUFFER_SIZE];
+    char pasvCommand[] = "pasv\n";
+    int h1, h2, h3, h4, p1, p2;
+
+    // Send PASV command
+    write(sockfd, pasvCommand, strlen(pasvCommand));
+    int response = read_response(sockfd, answer);
+    printf("Passive Mode Command Sent! Response: %d\n", response); // 227: Entering Passive Mode
+
+    if (response != SERVER_PASSIVE) {
+        printf("ERROR passive_mode: %s\n", answer);
+        return 1;
+    }
+
+    sscanf(answer, PASSIVE, &h1, &h2, &h3, &h4, &p1, &p2);
+    *port = p1 * 256 + p2;
+    sprintf(ip, "%d.%d.%d.%d", h1, h2, h3, h4);
+
+    return response;
+}
+
+int request_resource(int sockfd, char *resource) {
+    char fileCommand[5+strlen(resource)+1], answer[MAX_BUFFER_SIZE];
+    sprintf(fileCommand, "retr %s\n", resource);
+    write(sockfd, fileCommand, sizeof(fileCommand));
     return read_response(sockfd, answer);
+}
+
+int download_resource(int sockfdA, int sockfdB, char *filename) {
+    char answer[MAX_BUFFER_SIZE];
+    FILE *fd = fopen(filename, "wb"); // Create file for Writing the Downloaded content
+    if (fd == NULL) {
+        printf("Error creating file for writing.");
+        exit(-1);
+    }
+
+    char buffer[MAX_BUFFER_SIZE];
+    int nbytes;
+    while (nbytes = read(sockfdB, buffer, MAX_BUFFER_SIZE)) {
+        if (fwrite(buffer, nbytes, 1, fd) < 0) return 1;
+    }
+    fclose(fd);
+
+    return read_response(sockfdA, answer);
+}
+
+int close_connection(int sockfdA, int sockfdB) {
+    char answer[MAX_BUFFER_SIZE];
+    write(sockfdA, "quit\n", 5);
+    int response = read_response(sockfdA, answer);
+    if (response != SERVER_EXIT) {
+        printf("ERROR close_connection: Response %d", response);
+        return 1;
+    }
+    if (close(sockfdA) != 0) return 1;
+
+    if (close(sockfdB) != 0) return 1;
+
+    return 0;
 }
 
 
@@ -201,20 +267,15 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    /*
     printf("User: %s\n", url.user);
     printf("Password: %s\n", url.password);
     printf("Host: %s\n", url.host);
     printf("Resource: %s\n", url.resource);
     printf("File: %s\n", url.file);
     printf("IP: %s\n", url.ip);
-    */
 
     // Server address handling
-    int sockfd;
     struct sockaddr_in server_addr;
-    char buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
-    size_t bytes;
 
     bzero((char *) &server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
@@ -222,17 +283,61 @@ int main(int argc, char *argv[]) {
     server_addr.sin_port = htons(21); 
 
     // Open a TCP socket
-    int socketA;
-    if ((socketA = create_socket(url.ip, 21)) < 0) {
+    int socketA = create_socket(url.ip, 21);
+    char answer[MAX_BUFFER_SIZE];
+    int response = read_response(socketA, answer);
+    if (socketA < 0 || response != SERVER_READY_FOR_AUTH) {
         printf("Error creating socket: Socket to %s and port %d failed\n", url.ip, 21);
         return 1;
     }
+    printf("Socket Created! Response: %d\n", response); // 220: Server Ready for Aut
 
     // Authenticate
     if (authenticate(socketA, url.user, url.password) != SERVER_LOGINSUCCESS) {
-        printf("Error authenticating: Username %s and password %s\n", url.user, url.password);
+        printf("Error authenticate: response code %d\n", response);
         return 1;
     }
 
+    // Put in passive mode
+    int port;
+    char ip[MAX_BUFFER_SIZE]; // Store Passive IP and Port
+    if (passive_mode(socketA, ip, &port) != SERVER_PASSIVE) {
+        printf("Error entering passive mode\n");
+        return 1;
+    }
+
+    // Create socket using Passive mode IP and Port
+    int socketB = create_socket(ip, port);
+    if (socketB < 0) {
+        printf("Error creating socket: Socket to %s and port %d failed\n", ip, port);
+        return 1;
+    }
+
+    // Send resource request
+    response = request_resource(socketA, url.resource);
+    if (response != SERVER_READY_FOR_DOWNLOAD) {
+        printf("Error requesting resource %s in %s:%d\n", url.resource, ip, port);
+        exit(-1);
+    }
+
+    printf("Resource Requested! Response: %d\n", response); // 150
+
+    // Get resource
+    response = download_resource(socketA, socketB, url.file);
+    if (response != SERVER_DOWNLOAD_SUCCESS) {
+        printf("Error downloading file '%s' from '%s:%d'\n", url.file, ip, port);
+        exit(-1);
+    }
+
+    printf("Resource Downloaded! Response: %d\n", response); // 226
+
+    // Close connection
+    if (close_connection(socketA, socketB) != 0) {
+        printf("Sockets close error\n");
+        exit(-1);
+    }
+
+    printf("Connection Closed, Exiting...\n");
+    
     return 0;
 }
